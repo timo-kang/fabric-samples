21b640b3534b0ad8de30f584516982097e93f3ec
timo-kang <timo@redwit.io>
2023-06-19 09:53:18 +0900

    fix restore.sh

--- test-network/restore.sh
+++ test-network/restore.sh
@@ -1,18 +1,16 @@
 set -ev
+cp -r backup/organizations .
+cp -r backup/channel-artifacts .
+cp -r backup/system-genesis-block .
 
-COMPOSE_FILE_BASE=compose/docker/docker-compose-test-net.yaml
-COMPOSE_FILE_BASE_RESTORE=compose/docker/docker-compose-test-net-restore.yaml
+COMPOSE_FILE_BASE=compose/compose-test-net.yaml
+COMPOSE_FILE_BASE_RESTORE=compose/compose-test-net-restore.yaml
 
-#bringing network down and clearing volumes
-
-docker-compose -f ${COMPOSE_FILE_BASE} down
-
-docker volume prune
-
-docker network prune
+mv ${COMPOSE_FILE_BASE} ${COMPOSE_FILE_BASE}.bak
+cp ${COMPOSE_FILE_BASE_RESTORE} ${COMPOSE_FILE_BASE}
 
 #Bringing network Up with Previous Backup
-docker-compose -f ${COMPOSE_FILE_BASE_RESTORE}  up -d
+docker-compose -f ${COMPOSE_FILE_BASE}  up -d
 
 #All done...
 exit 1
